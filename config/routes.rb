827e314bee9a9ff91809e327297466a3534caed1
Nick-Damico <nickalan82@icloud.com>
2023-09-22 12:38:17 -0400

    Feat(WIP): Add Static Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
