2733092e1f2b5ecda340a67c77e1bff8d5933601
Nick-Damico <nickalan82@icloud.com>
2023-09-27 12:27:39 -0400

    Spec: Add Quiz validations

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -4,11 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
   subject { build(:quiz) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
   it { should have_many(:questions) }
 
   it 'has a valid factory' do
     expect(subject).to be_valid
   end
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
 end
