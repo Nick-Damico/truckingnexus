c952acb4c1c5f777c4bbfc9f232c931e880c9cf7
Nick-Damico <nickalan82@icloud.com>
2023-09-28 09:22:57 -0400

    Feat: Add optional to Question correct_answer association
    Question should not appear on actual quiz if no correct answer set

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  belongs_to :correct_answer, class_name: 'Answer', foreign_key: 'correct_answer_id'
+  belongs_to :correct_answer, class_name: 'Answer', foreign_key: 'correct_answer_id', optional: true
   belongs_to :quiz
   has_many :answers
 
   validates :content, presence: true
   validates :quiz, presence: true
+
+  accepts_nested_attributes_for :answers
 end
