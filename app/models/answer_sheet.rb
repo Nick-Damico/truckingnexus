40dedb9cf4dd02a0be22715564512bfd0e97fd8d
Nick-Damico <nickalan82@icloud.com>
2023-10-02 09:41:37 -0400

    Feat: Prepare for quiz by generating Questions for the AnswerSheet

--- app/models/answer_sheet.rb
+++ app/models/answer_sheet.rb
@@ -6,4 +6,20 @@ class AnswerSheet < ApplicationRecord
   has_many :answer_sheet_questions
   has_many :questions, through: :answer_sheet_questions
   has_many :answers, through: :answer_sheet_questions
+
+  def self.prep_for_quiz(user_quiz:)
+    generate_answer_sheet_questions(AnswerSheet.find_or_create_by(user_quiz:))
+  end
+
+  def self.generate_answer_sheet_questions(answer_sheet)
+    return nil unless answer_sheet && (quiz = answer_sheet.user_quiz.quiz)
+
+    quiz.questions.each do |question|
+      answer_sheet.answer_sheet_questions.create(question:)
+    end
+  end
+
+  def next_question
+    answer_sheet_questions.find_by(answer: nil)
+  end
 end
--- spec/models/answer_sheet_spec.rb
+++ spec/models/answer_sheet_spec.rb
@@ -3,8 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe AnswerSheet, type: :model do
+  let!(:quiz) { create(:quiz, :with_questions) }
+  let!(:user_quiz) { create(:user_quiz, quiz:) }
+
   it { should belong_to(:user_quiz) }
   it { should have_many(:answer_sheet_questions) }
   it { should have_many(:questions).through(:answer_sheet_questions) }
   it { should have_many(:answers).through(:answer_sheet_questions) }
+
+  describe '.prep_for_quiz' do
+    it 'generates AnswerSheet for UserQuiz' do
+      expect do
+        described_class.prep_for_quiz(user_quiz:)
+      end.to change(AnswerSheet, :count).by(1)
+    end
+
+    it 'generates AnswerSheetQuestion records for a quizzes questions' do
+      expected_count = user_quiz.quiz.questions.count
+      expect(expected_count).to_not be_nil
+      expect(expected_count).to_not be_zero
+      expect do
+        described_class.prep_for_quiz(user_quiz:)
+      end.to change(AnswerSheetQuestion, :count).by(expected_count)
+    end
+  end
+
+  describe '#next_question' do
+    it 'returns the next unanswered AnswerSheetQuestion' do
+      user_quiz.prep_for_quiz
+      answer_sheet = user_quiz.answer_sheet
+      unanswered_question = answer_sheet.next_question
+
+      expect(unanswered_question).to be_a(AnswerSheetQuestion)
+    end
+  end
 end
