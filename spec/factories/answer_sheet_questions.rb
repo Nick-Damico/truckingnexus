766afd8a92ffa9fcf8d9009e582ff36f5b5b9df4
Nick-Damico <nickalan82@icloud.com>
2023-10-02 09:40:09 -0400

    Refactor: AnswerSheet question records start unanswered

---
+++ spec/factories/answer_sheet_questions.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :answer_sheet_question do
+    question { nil }
+    answer { nil }
+    answer_sheet { nil }
+  end
+end
--- spec/models/answer_sheet_question_spec.rb
+++ spec/models/answer_sheet_question_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe AnswerSheetQuestion, type: :model do
-  it { should belong_to(:answer) }
+  it { should belong_to(:answer).optional(true) }
   it { should belong_to(:question) }
   it { should belong_to(:answer_sheet) }
 end
