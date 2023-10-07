57d8a3df5d987864ab609cc996cb9fa905572488
Nick-Damico <nickalan82@icloud.com>
2023-10-07 11:20:50 -0400

    Feat(WIP): Add UsersController#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'static_pages#home'
 
+  resources :answer_sheet_questions, only: %i[edit update]
   resources :companies, only: [:index]
   resources :quizzes, only: [:index]
   resources :user_quizzes, only: %i[new create show destroy]
-  resources :answer_sheet_questions, only: %i[edit update]
+  resources :users, only: %i[show]
 end
