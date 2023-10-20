bc177c0d1f6b254662a393fd2deee7438ef56b96
Nick-Damico <nickalan82@icloud.com>
2023-10-20 11:03:12 -0400

    Feat: Allow user to dismiss notification messages with animation

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,3 +1,22 @@
 import { Controller } from '@hotwired/stimulus'
+import { connectedMsg } from './utilities'
 
-export default class extends Controller {}
+export default class extends Controller {
+  static targets = ['container']
+
+  connect() {
+    connectedMsg('Flash Controller')
+  }
+
+  dismiss() {
+    if (this.hasContainerTarget) {
+      this.containerTarget.classList.add('flash-dismiss-animation')
+
+      this.containerTarget.addEventListener('transitionend', () => {
+        if (this.hasContainerTarget) {
+          this.containerTarget.remove()
+        }
+      })
+    }
+  }
+}
