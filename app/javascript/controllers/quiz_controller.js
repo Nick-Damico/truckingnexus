c433ccbbe719b79a206fbf327c360106505b4d54
Nick-Damico <nickalan82@icloud.com>
2023-11-26 07:39:09 -0500

    Feat: Enable submit button after question selected

--- app/javascript/controllers/quiz_controller.js
+++ app/javascript/controllers/quiz_controller.js
@@ -8,9 +8,27 @@ export default class extends Controller {
     connectedMsg('Quiz')
   }
 
-  enableSubmit() {
-    if (this.hasSubmitTarget) {
-      this.submitTarget.classList.remove('btn--disabled')
+  allowSubmit() {
+    if (this.hasSubmitTarget && this.answerSelected()) {
+      this.enableSubmit()
     }
+  }
+
+  answerSelected() {
+    let answerSelected = false
+
+    for (let i = 0; i < this.answerTargets.length; i++) {
+      if (this.answerTargets[i].checked) {
+        answerSelected = true
+        break
+      }
+    }
+
+    return answerSelected
+  }
+
+  enableSubmit() {
+    this.submitTarget.classList.remove('btn--disabled')
+    this.submitTarget.removeAttribute('disabled')
   }
 }
