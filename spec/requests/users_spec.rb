e090c53ba529ca3f997379b7e871894cec40dad2
Nick-Damico <nickalan82@icloud.com>
2023-11-28 20:59:46 -0500

    Spec(WIP): Coverage of route GET users#show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  subject(:user) { create(:user) }
+
+  before { sign_in user }
+
   describe 'GET /show' do
-    # it "returns http success" do
-    #   get "/users/show"
-    #   expect(response).to have_http_status(:success)
-    # end
+    it 'returns http success' do
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+    end
   end
 end
