ae12bce2eeaeb7e0f5d6deeb9afb98f2a473187f
Nick-Damico <nickalan82@icloud.com>
2023-12-27 21:47:31 -0600

    Feat: Allow user to set their current employer; WIP

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,15 +19,34 @@ RSpec.describe 'Users', type: :request do
     let(:params) { { user: { username: expected_username } } }
 
     it 'redirects to user show page' do
-      patch user_path(user), params: params
+      patch(user_path(user), params:)
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to user
     end
 
     it 'updates users name' do
-      patch user_path(user), params: params
+      patch(user_path(user), params:)
       expect(user.reload.username).to eq expected_username
       expect(user.reload.username).to_not be_nil
+    end
+
+    context 'current employer' do
+      before do
+        @current_employer = create(:company)
+        params[:user].merge!(
+          { 'employment_histories_attributes' =>
+            { '0' => { 'current' => 'true', 'employer_id' => @current_employer.id } } }
+        )
+
+        patch user_path(user), params: params
+      end
+
+      it 'sets the users current employer' do
+        expect(user.reload.current_employer).to eq(@current_employer)
+      end
+
+      xit 'updates the employment history record when the company is changed' do
+      end
     end
   end
 end
