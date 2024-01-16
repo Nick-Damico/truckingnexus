bb4b15f548c1878c35ac2dfce17fd905e61f9394
Nick-Damico <nickalan82@icloud.com>
2024-01-16 09:25:28 -0500

    Feat(model): Add scope :current_employer to fetch current employer employment_histories record

--- app/models/employment_history.rb
+++ app/models/employment_history.rb
@@ -3,4 +3,6 @@
 class EmploymentHistory < ApplicationRecord
   belongs_to :employee, class_name: 'User', foreign_key: :employee_id
   belongs_to :employer, class_name: 'Company', foreign_key: :employer_id
+
+  scope :current_employer, -> { find_by(current: true) }
 end
