5fd3932819441540b5a738d4e5efdf9e69f4f427
Nick-Damico <nickalan82@icloud.com>
2024-01-25 10:28:00 -0500

    fix(spec): Test for http status code; Not text

--- spec/requests/quizzes_spec.rb
+++ spec/requests/quizzes_spec.rb
@@ -6,10 +6,9 @@ RSpec.describe 'Quizzes', type: :request do
   # let!(:companies) { create_list(:company, 2) }
 
   describe 'GET /quizzes' do
-    it 'renders the index template' do
+    it 'responds with status success' do
       get quizzes_path
       expect(response).to have_http_status(200)
-      expect(response.body).to include('CDL Quizzes')
     end
   end
 end
