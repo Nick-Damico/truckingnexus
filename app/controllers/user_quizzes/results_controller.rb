a61124ebf66956eb3d581fbf1657c71fcb72571f
Nick-Damico <nickalan82@icloud.com>
2024-01-26 09:04:16 -0500

    feat(controller): Set user_quiz instance in before_action

--- app/controllers/user_quizzes/results_controller.rb
+++ app/controllers/user_quizzes/results_controller.rb
@@ -2,6 +2,14 @@
 
 module UserQuizzes
   class ResultsController < ApplicationController
+    before_action :set_user_quiz, only: %i[show]
+
     def show; end
+
+    private
+
+    def set_user_quiz
+      @user_quiz = UserQuiz.find(params[:id])
+    end
   end
 end
--- spec/requests/user_quizzes/results_spec.rb
+++ spec/requests/user_quizzes/results_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'UserQuizzes::Results', type: :request do
   describe 'GET /show' do
     it 'returns http success' do
-      get '/user_quizzes/results/show'
+      user_quiz = create(:user_quiz, :completed)
+      get user_quizzes_result_path(user_quiz)
+
       expect(response).to have_http_status(:success)
     end
   end
