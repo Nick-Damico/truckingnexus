d9ca18cbbe55bb6cfbacafae9267f77f04f46967
Nick-Damico <nickalan82@icloud.com>
2024-01-28 10:35:41 -0500

    refactor(helper): Render button with icon

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 module QuizzesHelper
+  include ApplicationHelper
   def answer_index_to_letter(idx)
     int_to_char(idx)
   end
 
-  def quiz_start_button(quiz:)
-    button_to(user_quizzes_path(user_quiz: { quiz_id: quiz.id, user_id: current_user.id }),
+  def quiz_start_button(quiz:, user: current_user, **kwargs)
+    text = kwargs[:text] || 'Start Quiz'
+    icon_name = kwargs[:icon] || 'right_arrow'
+
+    button_to(user_quizzes_path(user_quiz: { quiz_id: quiz.id, user_id: user.id }),
               class: 'btn btn--save') do
-      content_tag(:span, 'Start Quiz') +
-        content_tag(:span,
-                    raw('<svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/> </svg>'))
+      content_tag(:span, text, class: 'mr-2') +
+        content_tag(:span, render_icon(icon_name, classes: 'w-4'))
     end
   end
 end
