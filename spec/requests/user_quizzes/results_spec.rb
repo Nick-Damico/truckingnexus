d82390461865d2245d155f75cded1bbbb9c016ad
Nick-Damico <nickalan82@icloud.com>
2024-02-02 11:59:51 -0500

    fix(spec): Authenticate User before test suite

--- spec/requests/user_quizzes/results_spec.rb
+++ spec/requests/user_quizzes/results_spec.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'UserQuizzes::Results', type: :request do
+  let(:user) { create(:user) }
+
+  before { sign_in(user) }
+
   describe 'GET /show' do
     it 'returns http success' do
       user_quiz = create(:user_quiz, :completed)
