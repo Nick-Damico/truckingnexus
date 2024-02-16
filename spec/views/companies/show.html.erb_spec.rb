30362de480eaa2271214ca021b635ebc1c035c32
Nick-Damico <nickalan82@icloud.com>
2024-02-16 16:00:19 -0500

    feat(view): add conditional no reviews content block

--- spec/views/companies/show.html.erb_spec.rb
+++ spec/views/companies/show.html.erb_spec.rb
@@ -24,33 +24,44 @@ RSpec.describe 'companies/show', type: :view do
     expect(rendered).to have_link 'Review', href: new_company_review_path(subject)
   end
 
-  context 'review' do
-    let(:company) { create(:company, :with_reviews) }
+  context 'without review' do
+    it 'displays a no reviews message' do
+      assign(:company, subject)
+
+      render
+
+      expect(rendered).to match(/no one has left a review yet/i)
+      expect(rendered).to have_content('0 Reviews')
+    end
+  end
+
+  context 'with review' do
     before do
-      assign(:company, company)
+      subject.reviews << create_list(:review, 1)
+      assign(:company, subject)
 
       render
     end
 
     it 'displays reviewer count' do
-      expected_content = "#{company.reviews.count} #{'Review'.pluralize(company.reviews.count)}"
+      expected_content = "#{subject.reviews.count} #{'Review'.pluralize(subject.reviews.count)}"
       expect(rendered).to have_content(expected_content)
     end
 
-    it 'displays reviewers username' do
-      expect(rendered).to have_content(company.reviews.first.reviewer.username)
+    it 'displays review username' do
+      expect(rendered).to have_content(subject.reviews.first.reviewer.username)
     end
 
     it 'displays review title' do
-      expect(rendered).to have_content(company.reviews.first.title)
+      expect(rendered).to have_content(subject.reviews.first.title)
     end
 
     it 'displays reviewers rating' do
-      expect(rendered).to have_content(company.reviews.first.rating.to_f)
+      expect(rendered).to have_content(subject.reviews.first.rating.to_f)
     end
 
     it 'displays review content' do
-      expect(rendered).to have_content(company.reviews.first.content)
+      expect(rendered).to have_content(subject.reviews.first.content)
     end
   end
 
