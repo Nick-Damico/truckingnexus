2fa1845a021ea153766b3728c6fdefa28358e75c
Nick-Damico <nickalan82@icloud.com>
2024-02-20 16:07:56 -0500

    refactor(styles): remove styling of flash :warning level
    Changing this to stick to 3 essential flash message levels, :notice for information, :success for successfully processing a request, and :alert for warning of critcal errors.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,7 @@ module ApplicationHelper
     klasses = ['bg-flash']
     case key.to_sym
     when :alert
-      klasses << 'bg-flash--warning'
+      klasses << 'bg-flash--error'
     when :info
       klasses << 'bg-flash--info'
     when :notice
