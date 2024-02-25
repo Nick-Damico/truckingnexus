1e1075196a8717b922a675fdd71191f6a9ef5c73
Nick-Damico <nickalan82@icloud.com>
2024-02-25 08:47:05 -0500

    feat: add reviews controllers
    Add base controller for polymorphic association Reviews:
    - Add ReviewsController with basic actions and before action hooks.
    - Add Companies/ReviewsController for overriding with specific behaviors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,40 @@
 # frozen_string_literal: true
 
 class ReviewsController < ApplicationController
-  before_action :selected_reviewable, only: %i[index]
+  before_action :set_review, only: %i[show]
+  before_action :set_reviewable, only: %i[index new create]
 
   def index
-    @reviews = @company.reviews
+    @reviews = @reviewable.reviews
   end
 
   def show; end
 
-  def selected_reviewable
-    @company = Company.find(params[:company_id])
+  def new
+    @review = @reviewable.reviews.new(reviewer: current_user)
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to polymorphic_url([@reviewable, @review]), notice: 'Review successfully saved'
+    else
+      render :new, alert: @review.errors.full_messages
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rating, :reviewable_type, :reviewable_id, :reviewer_id)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def set_reviewable
+    raise NotImplementedError, "#{params[:reviewable_type]} has not implemented ##{__method__}"
   end
 end
