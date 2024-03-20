fa0e33e1a3f20e5b550aeb91b0917934a3176318
Nick-Damico <nickalan82@icloud.com>
2024-03-20 17:22:43 -0400

    feat(helper): add #rating display for companies

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module CompaniesHelper
+  def rating(company)
+    return 'No Reviews' unless company.reviews.exists?
+
+    pluralize(company.rating.to_i, 'star')
+  end
 end
