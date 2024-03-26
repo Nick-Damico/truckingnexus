9289cc11136a0b7679f04d52e5144a356896ee55
Nick-Damico <nickalan82@icloud.com>
2024-03-26 13:44:37 -0400

    chore(route): cleanup routes; add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: :registrations }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'static_pages#home'
 
+  # User registartion handled by RegistrationsController
+  devise_for :users, controllers: { registrations: :registrations }
   resources :users, only: %i[show update]
 
   # QUIZ ROUTES
