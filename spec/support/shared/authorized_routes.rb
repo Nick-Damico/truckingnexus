39882b41a09d5032638a0da3595e68bdeeb0de50
Nick-Damico <nickalan82@icloud.com>
2024-03-27 12:51:47 -0400

    feat(spec): add shared example authorized routes
    - Not happy with the name ATM
    - This a basic check that the controller redirects if an unauthorized
    user is trying to access a resource

---
+++ spec/support/shared/authorized_routes.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'Authorized Routes' do
+  let(:unauthorized_user) { create(:user) }
+  let(:authorized_routes) { [] } # Add routes that should be protected with authorization
+
+  before { sign_in unauthorized_user }
+
+  context 'when trying to access without authorization' do
+    it 'responds with HTTP status redirect(302)' do
+      authorized_routes.each do |route|
+        get route
+
+        expect(response).to have_http_status(:redirect)
+        # expect(response).to redirect_to(redirect_route) # Adjust this if your redirection logic differs
+      end
+    end
+
+    it 'displays a flash message indicating unauthorized access' do
+      authorized_routes.each do |route|
+        get route
+
+        expect(flash[:alert]).to eq('Access denied. You are not authorized to view this page.') # Adjust the flash message as per your application
+      end
+    end
+  end
+end
