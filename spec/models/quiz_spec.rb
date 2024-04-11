b844afde652bb6ade6e342f8a744b9e652f030ba
Nick-Damico <nickalan82@icloud.com>
2024-04-11 14:11:47 -0400

    chore(spec): organize validation examples

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -5,14 +5,14 @@ require 'rails_helper'
 RSpec.describe Quiz, type: :model do
   subject { build(:quiz) }
 
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
   it { should belong_to(:author) }
   it { should have_many(:questions) }
   it { should have_many(:users).through(:user_quizzes) }
 
   context 'Validations' do
     it { should validate_presence_of(:author) }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 
   it 'defaults to Draft true for new records' do
