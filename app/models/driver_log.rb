f301dc04cae9725d163091774eb43b6a49a03dad
Nick-Damico <nickalan82@icloud.com>
2024-07-21 11:33:21 -0400

    feat: init start/end times on new driver logs
    Driver logs will initialize to the beginning of the day and end of the
    day. A log represents a drivers duty status between a 24 hour period. In
    the future the driver will be able to record backlog records where they
    specify a day. The end and start times will still be set by the
    application.

--- app/models/driver_log.rb
+++ app/models/driver_log.rb
@@ -3,4 +3,15 @@
 class DriverLog < ApplicationRecord
   belongs_to :driver, class_name: 'User'
   has_many :duty_statuses
+
+  after_initialize :set_start_and_end_times
+
+  private
+
+  def set_start_and_end_times
+    cur_time = Time.zone.now
+
+    self.started_at = cur_time.beginning_of_day
+    self.ended_at = cur_time.end_of_day
+  end
 end
--- spec/models/driver_log_spec.rb
+++ spec/models/driver_log_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe DriverLog, type: :model do
 
   it { should belong_to(:driver) }
   it { should have_many(:duty_statuses) }
+
+  describe 'after_initialize' do
+    it 'sets started_at to the beginning of the day' do
+      driver_log = build(:driver_log)
+      expect(driver_log.started_at.to_s).to eq(Time.zone.now.beginning_of_day.to_s)
+    end
+
+    it 'sets ended_at to the end of the day' do
+      driver_log = build(:driver_log)
+      expect(driver_log.ended_at.to_s).to eq Time.zone.now.end_of_day.to_s
+    end
+  end
 end
