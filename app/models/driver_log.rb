9365c2b3c9c925c7405df533c191b83e922b3adc
Nick-Damico <nickalan82@icloud.com>
2024-07-21 15:46:02 -0400

    feat: require 24 hour clock start/end time

--- app/models/driver_log.rb
+++ app/models/driver_log.rb
@@ -6,10 +6,12 @@ class DriverLog < ApplicationRecord
 
   after_initialize :set_start_and_end_times
 
+  validates :started_at, :ended_at, presence: true
+
   private
 
   def set_start_and_end_times
-    cur_time = Time.zone.now
+    cur_time = Time.current
 
     self.started_at = cur_time.beginning_of_day
     self.ended_at = cur_time.end_of_day
--- spec/models/driver_log_spec.rb
+++ spec/models/driver_log_spec.rb
@@ -8,15 +8,20 @@ RSpec.describe DriverLog, type: :model do
   it { should belong_to(:driver) }
   it { should have_many(:duty_statuses) }
 
+  describe 'validations' do
+    it { should validate_presence_of(:started_at) }
+    it { should validate_presence_of(:ended_at) }
+  end
+
   describe 'after_initialize' do
     it 'sets started_at to the beginning of the day' do
       driver_log = build(:driver_log)
-      expect(driver_log.started_at.to_s).to eq(Time.zone.now.beginning_of_day.to_s)
+      expect(driver_log.started_at.to_s).to eq(Time.current.beginning_of_day.to_s)
     end
 
     it 'sets ended_at to the end of the day' do
       driver_log = build(:driver_log)
-      expect(driver_log.ended_at.to_s).to eq Time.zone.now.end_of_day.to_s
+      expect(driver_log.ended_at.to_s).to eq Time.current.end_of_day.to_s
     end
   end
 end
