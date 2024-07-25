70c90d2e37f7d29776ecd0ebaaef27daa6eb1540
Nick-Damico <nickalan82@icloud.com>
2024-07-25 12:36:53 -0400

    feat: validate log start/end times are 24-hr range

--- app/models/driver_log.rb
+++ app/models/driver_log.rb
@@ -9,8 +9,6 @@ class DriverLog < ApplicationRecord
 
   after_initialize :set_start_and_end_times
 
-  validates :started_at, :ended_at, presence: true
-
   private
 
   def set_start_and_end_times
@@ -18,5 +16,12 @@ class DriverLog < ApplicationRecord
 
     self.started_at = cur_time.beginning_of_day
     self.ended_at = cur_time.end_of_day
+  end
+
+  def start_and_end_dates
+    return unless started_at.present? && ended_at.present?
+    return if ((ended_at - started_at) / 1.hour.to_i).ceil == 24
+
+    errors.add(:base, 'Start and End times must be within a 24-hour period')
   end
 end
--- spec/models/driver_log_spec.rb
+++ spec/models/driver_log_spec.rb
@@ -11,6 +11,28 @@ RSpec.describe DriverLog, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:started_at) }
     it { should validate_presence_of(:ended_at) }
+
+    describe '#start_and_end_times' do
+      context 'start and end times represent a 24-hour peroid' do
+        it 'does not set an error if times are valid' do
+          driver_log = build(:driver_log)
+          driver_log.started_at = Time.current.beginning_of_day
+          driver_log.ended_at = Time.current.end_of_day
+
+          expect(driver_log).to be_valid
+          expect(driver_log.errors[:base]).to_not include('Start and End times must be within a 24-hour period')
+        end
+
+        it 'sets errors if times are invalid' do
+          driver_log = build(:driver_log)
+          driver_log.started_at = Time.current.beginning_of_day
+          driver_log.ended_at = 1.day.from_now.end_of_day
+
+          expect(driver_log).to be_invalid
+          expect(driver_log.errors[:base]).to include('Start and End times must be within a 24-hour period')
+        end
+      end
+    end
   end
 
   describe 'after_initialize' do
