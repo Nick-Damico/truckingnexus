05681091cf884cd80ca1bd6eeea7b23e4909ee52
Nick-Damico <nickalan82@icloud.com>
2024-07-27 16:03:28 -0400

    feat: add DateRange#include?
    Checks if specified time is covered by the time range

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -8,6 +8,10 @@ class DateRange
     @end_date = end_date
   end
 
+  def include?(time)
+    (start_date..end_date).cover?(time)
+  end
+
   def total_hours
     (end_date - start_date).ceil / 1.hour
   end
