cfeff82ec79917be2b2f9c0b4aeea990ea7b48e9
Nick-Damico <nickalan82@icloud.com>
2024-07-29 15:21:03 -0400

    feat: redirect to user dashboard on sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if resource.present?
+      resource || root_path
+    else
+      super
+    end
+  end
 end
