d9da6c8138ce276da38c14de6b8eed1dc2aab359
Nick-Damico <nickalan82@icloud.com>
2024-08-09 20:21:41 -0400

    refactor: helper #rating display icons or text

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 module CompaniesHelper
-  def rating(company)
+  def rating(company, display_with_icons: true)
     return 'No Reviews' unless company.reviews.any?
 
-    pluralize(company.rating.to_i, 'star')
+    if display_with_icons
+      rating_stars(rating: company.rating)
+    else
+      pluralize(company.rating.to_i, 'star')
+    end
   end
 end
