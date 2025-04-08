8c7473ee369e1fa42c910953c6000059196fa71f
Nick-Damico <nickalan82@icloud.com>
2025-04-08 19:44:38 -0400

    feat: post `rails app:update` 7.2
    TODO:
      - enable and test `config.load_defaults 7.2`

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'stylesheets')
+Rails.application.config.assets.paths << Rails.root.join("vendor", "stylesheets")
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -10,4 +10,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'stylesheets'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
