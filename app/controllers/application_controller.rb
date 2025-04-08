c87c3df6f3709ed71d2c4261da355a7448955ee8
Nick-Damico <nickalan82@icloud.com>
2025-04-08 00:11:19 -0400

    feat: add rails 7.2 browser guard
    change:
      - permit modern browsers only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Allow only browsers natively supporting webp images, web push, badges, import maps, CSS nesting + :has
+  allow_browser versions: :modern
+
   def after_sign_in_path_for(resource)
     if resource.present?
       resource || root_path
