d3ec3d2b879dadb492dbe7eb73688a607938c827
Nick-Damico <nickalan82@icloud.com>
2025-04-22 14:11:41 -0400

    feat(spec): scope button to .quiz-card element
    This uses Capybara#string to parse the rendered DOM content string
    returned from `rendered`.

--- spec/views/quizzes/index.html.erb_spec.rb
+++ spec/views/quizzes/index.html.erb_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'quizzes/index', type: :view do
   let!(:user) { create(:user) }
-  let!(:quiz) { create(:quiz, :with_questions, name: "Ruby Basics") }
+  let!(:quiz) { create(:quiz, :with_questions, :published, name: "Ruby Basics") }
 
   before do
     sign_in user
@@ -16,7 +16,8 @@ RSpec.describe 'quizzes/index', type: :view do
     expect(rendered).to include("Ruby Basics")
   end
 
-  it "shows a start button if the user has not started the quiz" do
-    expect(rendered).to have_content("Start")
+  it "shows a Start button inside a quiz-card" do
+    dom = Capybara.string(rendered)
+    expect(dom).to have_selector(".quiz-card button", text: "Start Quiz")
   end
 end
