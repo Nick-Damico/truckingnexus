bb76c7bc4422fc988c3a56a6a1f67646f69ee3bc
Nick-Damico <nickalan82@icloud.com>
2025-04-25 09:19:25 -0400

    feat(model): add locatable w/validations

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,5 +1,8 @@
 class Geolocation < ApplicationRecord
+  belongs_to :locatable, polymorphic: true
+
   validates :name, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+  validates :locatable, presence: true
 end
--- spec/models/geolocation_spec.rb
+++ spec/models/geolocation_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Geolocation, type: :model do
+  it { should belong_to(:locatable) }
+
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:latitude) }
   it { should validate_presence_of(:longitude) }
+  it { should validate_presence_of(:locatable) }
 end
