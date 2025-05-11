33fb68c42209a3d16b3d4fe5933b2bec12cfd8da
Nick-Damico <nickalan82@icloud.com>
2025-05-11 08:26:18 -0400

    feat: geolocations have many notes

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,4 +1,6 @@
 class Geolocation < ApplicationRecord
+  has_many :notes, dependent: :destroy
+
   validates :name, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
--- spec/models/geolocation_spec.rb
+++ spec/models/geolocation_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Geolocation, type: :model do
+  it { should have_many(:notes) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:latitude) }
   it { should validate_presence_of(:longitude) }
